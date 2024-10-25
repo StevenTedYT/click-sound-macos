c871805f238ac443a444897f4bee929da4fd7ba9
Steven <45540516+StevenTedYT@users.noreply.github.com>
2024-10-25 16:46:04 +0200

    feat: add volume control

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from pynput import mouse
 import pygame
 
 pygame.mixer.init()
+volume = 0.25
 
 click_sound_input = pygame.mixer.Sound('click_sound_input.wav')
 click_sound_output = pygame.mixer.Sound('click_sound_output.wav')
+
+click_sound_input.set_volume(volume)
+click_sound_output.set_volume(volume)
 
 def on_click(x, y, button, pressed):
     if pressed:
